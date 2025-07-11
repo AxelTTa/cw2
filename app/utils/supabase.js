2c055f440286f0d818275ecf8d1296d095bb5598
samy2700 <samy.ait-alla@efrei.net>
2025-07-11 16:52:56 +0200

    supabse keys

--- app/utils/supabase.js
+++ app/utils/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://xabjpewtqmddevpbmxnl.supabase.co';
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhhYmpwZXd0cW1kZGV2cGJteG5sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMzI0NTcsImV4cCI6MjA2NzgwODQ1N30.O694Ml-hEecjK8HmuXOhCgdfHFcoktI6ZfThHcVmyAE"
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
